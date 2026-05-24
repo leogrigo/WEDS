9d7ea0662d4b5fd6f0498e8b23cc10f25e8cd06a
Mar-cot <75980836+Mar-cot@users.noreply.github.com>
2026-05-24 16:02:03 +0200

    Switched to adafruitBME680 Library

--- code/lib/weds_sensors/WedsSensors.cpp
+++ code/lib/weds_sensors/WedsSensors.cpp
@@ -100,6 +100,10 @@ bool checkBmeSensorStatus(void) {
   return true;
 }
 
+const uint8_t bsec_config[] = {
+    #include <config/generic_33v_3s_28d/bsec_iaq.txt>
+};
+
 bool weds_sensors_begin(){
     if(!Wire1.begin(WEDS_I2C_SDA, WEDS_I2C_SCL)){
         Serial.println("[SENSOR] I2C init failed");
@@ -108,17 +112,16 @@ bool weds_sensors_begin(){
 
     bmeSensor.begin(I2C_ADDRESS, Wire1);
 
-    bsec_virtual_sensor_t sensorList[7] = {
+    bsec_virtual_sensor_t sensorList[4] = {
         BSEC_OUTPUT_RAW_PRESSURE,
         BSEC_OUTPUT_RAW_GAS,
-        BSEC_OUTPUT_STATIC_IAQ,
-        BSEC_OUTPUT_CO2_EQUIVALENT,
-        BSEC_OUTPUT_BREATH_VOC_EQUIVALENT,
         BSEC_OUTPUT_SENSOR_HEAT_COMPENSATED_TEMPERATURE,
         BSEC_OUTPUT_SENSOR_HEAT_COMPENSATED_HUMIDITY,
     };
 
-    bmeSensor.updateSubscription(sensorList, 7, BSEC_SENSOR_RATE);
+    bmeSensor.setConfig(bsec_config);
+
+    bmeSensor.updateSubscription(sensorList, 4, BSEC_SENSOR_RATE);
 
     return checkBmeSensorStatus();
 
